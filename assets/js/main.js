e79f74cc2eb8df34e516476b1a0b5b7924a58484
newton mathias <newtymathias@gmail.com>
2022-01-18 15:02:14 +0300

    navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,25 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav__menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav__close');
+
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
